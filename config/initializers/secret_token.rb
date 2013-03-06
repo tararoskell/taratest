c717872f8d409d64e32ab6516392ab216896d18a
Tara Roskell <tara@troskell.demon.co.uk>
2013-03-06 18:31:21 +0000

    another test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taratest::Application.config.secret_token = '84223eaf54d267dfbd6f78d0ab4c90efafa8ff0bceeb4409109c709d8befbd7559be3cb119951c2242897b0e7f83b0a8cccb47c5b8d9ad77ee6bdc19e1eb8eaa'
